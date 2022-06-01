b8ad5b22c1e6004f73c5f911c17b7f4087e45cdd
Jaideep <jaideep.phalswal@glamplus.in>
2022-06-01 14:04:42 +0530

    added scroll to brand

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,6 +23,33 @@ function Navbar() {
         <Image src="/logo.png" width="44" height="28" />
         <Image src="/menu.png" width="17" height="17" />
       </div>
+      <div className="fixed top-0 right-0 bottom-0 left-0 backdrop-blur-sm z-10">
+        <ul className="absolute top-0 right-0 bottom-0 w-10/12 bg-white drop-shadow-2xl">
+          <li className="border-b border-slate-100">
+            <Link href="#">
+              <a className="block p-4">Features</a>
+            </Link>
+          </li>
+          <li className="border-b border-slate-50">
+            <Link href="#">
+              <a className="block p-4">Features</a>
+            </Link>
+          </li>
+          <li className="border-b border-slate-100">
+            <Link href="#">
+              <a className="block p-4">Features</a>
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="absolute top-2 right-0 bottom-0 left-2 w-[30px] h-[20px]"
+          onClick={() => setToggleNavbar(false)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path d="M24 3.752l-4.423-3.752-7.771 9.039-7.647-9.008-4.159 4.278c2.285 2.885 5.284 5.903 8.362 8.708l-8.165 9.447 1.343 1.487c1.978-1.335 5.981-4.373 10.205-7.958 4.304 3.67 8.306 6.663 10.229 8.006l1.449-1.278-8.254-9.724c3.287-2.973 6.584-6.354 8.831-9.245z" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
--- components/landingPage/Customers.js
+++ components/landingPage/Customers.js
@@ -1,33 +1,41 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
 function Customers() {
+  const [pauseAnimation, setPauseAnimation] = useState(false);
   return (
-    <div className="min-h-[800px] h-full py-10 max-w-[1800px] m-auto">
-      <h1 className="font-[600] text-[24px] leading-[25px] text-center sm:text-[28px] sm:leading-[34px] md:text-[34px] md:leading-[40px] lg:text-[60px] lg:leading-[74px]">
+    <div className="min-h-[600px] h-full py-10 max-w-[1800px] m-auto xl:min-h-[800px]">
+      <h1 className="font-[600] text-[24px] leading-[25px] mb-10 text-center sm:text-[28px] sm:leading-[34px] md:text-[34px] md:leading-[40px] lg:text-[60px] lg:leading-[74px]">
         See why 50k+ Customers <br /> Love Us?
       </h1>
-      <div className="flex flex-wrap gap-10 justify-around px-10 mt-14">
-        {reviews.map((review) => {
-          return (
-            <div
-              key={review.imageUrl}
-              className="w-[402px] rounded-[20px] customer-review-shadow p-6 xl:w-[462px] xl:p-10"
-            >
-              <div className="pb-6">
-                <Image src="/LandingPageImg/comma.png" className="" width="60" height="40" />
-              </div>
-              <p className="text-[16px] xl:text-[18px]">{review.description} </p>
-              <div className="flex items-center gap-6 mt-6">
-                <Image src="/LandingPageImg/avtar.png" className="rounded-full" width="76" height="76" />
-                <div>
-                  <h1 className="text-[18px] font-[500] xl:text-[20px]">{review.name} </h1>
-                  <p className="text-[#828282] text-[16px] xl:text-[18px]">{review.place}</p>
+      <div
+        className={`tech-slideshow min-w-fit min-h-[350px] p-2 m-auto xl:min-h-[500px] xl:w-[1540px]`}
+        onClick={() => setPauseAnimation(!pauseAnimation)}
+      >
+        <div
+          className={`mover-1 lg:relative ${pauseAnimation ? "pause-animation" : "start-animation"} xl:animation-none`}
+        >
+          {reviews.map((review) => {
+            return (
+              <div
+                key={review.imageUrl}
+                className="w-[402px] rounded-[20px] customer-review-shadow p-6 xxl:w-[462px] xl:p-10"
+              >
+                <div className="pb-6">
+                  <Image src="/LandingPageImg/comma.png" className="" width="60" height="40" />
+                </div>
+                <p className="text-[12px] md:text-[14px] xl:text-[18px]">{review.description} </p>
+                <div className="flex items-center gap-6 mt-6">
+                  <Image src="/LandingPageImg/avtar.png" className="rounded-full" width="76" height="76" />
+                  <div>
+                    <h1 className="text-[18px] font-[500] xl:text-[20px]">{review.name} </h1>
+                    <p className="text-[#828282] text-[16px] xl:text-[18px]">{review.place}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
